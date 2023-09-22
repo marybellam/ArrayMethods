861ca00271f52d7e4c5272f874f148715b54229c
marybellam <mmartinez31@smith.edu>
2023-09-22 08:08:57 -0400

    commit

---
+++ src/ClimateStudy.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ClimateStudy {
+    /** The main method reads from a file and prints the contents. */
+  public static void main(String[] args) {
+    Scanner file = null;
+    String[] files = {"YUMA_2023.txt","YUMA_2022.txt","YUMA_2021"};
+
+    for(int i =0;i<=files.length-1;i++){
+    try {
+      file = new Scanner(new File(files[i]));
+    } catch (FileNotFoundException e) {
+      System.err.println("Cannot locate file.");
+      System.exit(-1);
+    }
+    while (file.hasNextLine()) {
+      String line = file.nextLine();
+      String[] fields = line.split("\\s+");
+      String date = fields[1];
+      float temperature = Float.valueOf(fields[8]);
+      System.out.println("On " + date + " the temperature was " + temperature + " degrees Celsius.");
+    }
+    
+    file.close();
+    }
+  }
+}
